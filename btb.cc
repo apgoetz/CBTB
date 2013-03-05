9aa0314cca76cdba783453fb330b7f9c9a730524
Andy Goetz <andy@andygoetz.org>
2013-03-05 15:34:57 -0800

    Added functions for btb testing

--- btb.cc
+++ btb.cc
@@ -1,4 +1,15 @@
 #ifndef __BTB_CC__
 #define __BTB_CC__
 // this contains the btb preprocessor
+
+uint target_predict(const branch_record_c *br)
+{
+	return 0;
+}
+
+void target_update(const branch_record_c *br, uint actual_addr)
+{
+}
+
 #endif
+
