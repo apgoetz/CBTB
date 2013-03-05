1f7c6c46f37173553d243c2a332382a052ee237f
Andy Goetz <andy@andygoetz.org>
2013-03-05 15:28:24 -0800

    Added functions to alpha.cc

--- alpha.cc
+++ alpha.cc
@@ -1,4 +1,14 @@
 #ifndef __ALPHA_CC__
 #define __ALPHA_CC__
-// this contains the alpha preprocessor
+
+bool predict(const branch_record_c *br, uint * addr)
+{
+	return false;
+}
+
+void update(const branch_record_c *br, bool taken, uint actual_addr)
+{
+	
+}
+
 #endif
