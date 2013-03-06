67f354984e40b0fd20217d3d49026a9833a2ad4d
Andy Goetz <andy@andygoetz.org>
2013-03-05 18:53:30 -0800

    Modified predictor so alpha is only called for branches

--- predictor.cc
+++ predictor.cc
@@ -5,24 +5,31 @@
 #include "alpha.cc"
 #include "btb.cc"
 static void on_exit(void);
+static void init(void);
 static long int timescalled= 0;
+
+
 bool PREDICTOR::get_prediction(
 	const branch_record_c* br, 
 	const op_state_c* os, 
 	uint *predicted_target_address)
 {
 	// need to only run this code once.
-	static int hooked_exit = 0;
-	if(!hooked_exit){
-		hooked_exit++;
-		atexit(on_exit);
+	static int initial_run = 1;
+	if(!initial_run){
+		initial_run = 0;
+		init();
 	}
-	bool taken = alpha_predict(br);
+	bool taken = true;
+
+	if (br->is_conditional)
+		taken = alpha_predict(br);
+
 	// the predictor is only checked if the branch was taken, or
 	// it was unconditional. Therefore, we only need to call the
 	// target predictor if we think this was a taken branch, or if
 	// the branch is unconditional
-	if (taken || !br->is_conditional)
+	if (taken)
 		*predicted_target_address = target_predict(br);
 
 	
@@ -38,12 +45,20 @@ void PREDICTOR::update_predictor(
 	const branch_record_c* br, 
 	const op_state_c* os, bool taken, uint actual_target_address)
 {
+
 	target_update(br, actual_target_address);
-	alpha_update(br, taken);
+
+	if (br->is_conditional)
+		alpha_update(br, taken);
 
 }
 static void on_exit(void)
 {
 	fprintf(stderr, "I got called %ld\n", timescalled);
 	// do nothing
+}
+
+static void init(void)
+{
+	atexit(on_exit);
 }
