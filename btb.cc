b5e2e526c91985a192a1129add2d19553fcb8faa
Andy Goetz <andy@andygoetz.org>
2013-03-07 14:30:29 -0800

    Made btb a class

--- btb.cc
+++ btb.cc
@@ -3,14 +3,8 @@
 
 #include <stdint.h>
 #include <stdlib.h>
-int BTB_BITSIZE = 4;
-int BTB_NUM_WAYS = 4;
-int BTB_SIZE;
-int BTB_TAG_SIZE;
-uint *btb_buffer;
-uint *btb_tags;
-uint *btb_lru;
 static uint lastcall;
+
 int log2(int value)
 {
 	value--;
@@ -23,48 +17,98 @@ int log2(int value)
 	}
 	return 0;
 }
+
+class BTB_CACHE {
+private:
+	size_t indexbits;
+	size_t numways;
+	size_t btbsize;
+	size_t tagsize;
+	uint* btb_buffer;
+	uint* btb_tags;
+	uint* btb_lru;
+public:
+	BTB_CACHE(int indexbits = 4, int numways = 1) : indexbits(indexbits), numways(numways), btbsize(1 << indexbits), tagsize(32-indexbits) {
+		btb_buffer = (uint*)malloc(btbsize*numways*sizeof(uint));
+		btb_tags = (uint*)malloc(btbsize*numways*sizeof(uint));
+		btb_lru = (uint*)malloc(btbsize*sizeof(uint));		
+
+		for(int i = 0; i < indexbits*numways; i++) {
+			btb_tags[i] = 0xffffffff;
+		}
+		for(uint i = 0; i < btbsize; i++) {
+			btb_lru[i] = 0;
+		}
+	}
+	virtual ~BTB_CACHE() {
+		free(btb_buffer);
+		free(btb_tags);
+		free(btb_lru);
+	}
+	virtual bool predict(uint instr, uint &target) {
+		uint index = instr & ((1 << indexbits) - 1);
+		uint shiftedindex = index * numways;
+		uint tag = instr >> indexbits;
+		
+		for(uint i = 0; i < numways; i++) {
+			if(btb_tags[shiftedindex+i] == tag) {
+				target =  btb_buffer[shiftedindex+i];
+				return true;
+			}
+		}
+		return false;
+	}
+	virtual void update(uint addr, uint target) {
+		uint index = addr & ((1 << indexbits) - 1);
+		uint shiftedindex = index * numways;
+		uint tag = addr >> indexbits;
+		
+		for (uint i = 0; i < numways; i++) {
+			if (btb_tags[shiftedindex+i] == tag){
+				return;
+			}
+		}	
+
+		// insert into table
+		btb_tags[shiftedindex + btb_lru[index]] = tag;
+		btb_buffer[shiftedindex+ btb_lru[index]] = target;
+		btb_lru[index]++;
+		btb_lru[index] = btb_lru[index] % numways;
+	}
+
+	virtual uint size(void) {
+		uint size = 0;
+		size += btbsize * numways * tagsize;
+		size += btbsize * numways* 32;
+		if(numways > 1)
+			size += btbsize*log2(numways);
+		return size;
+	}
+};
+
+
+BTB_CACHE *maincache;
 uint btb_predict(const branch_record_c *br)
 {
-	uint addr = br->instruction_addr;
-	uint index = addr & ((1 << BTB_BITSIZE) - 1);
-	uint shiftedindex = index * BTB_NUM_WAYS;
-	uint tag = addr >> BTB_BITSIZE;
-
 	if(br->is_return) {
 		return lastcall;
 	}
 
-	for(int i = 0; i < BTB_TAG_SIZE; i++) {
-		if(btb_tags[shiftedindex+i] == tag)
-			return btb_buffer[shiftedindex+i];		
-	}
-	return br->instruction_next_addr;
+	uint target;
+	if(!maincache->predict(br->instruction_addr, target))
+		target = br->instruction_next_addr;
+
+	return target;
 }
 
 void btb_update(const branch_record_c *br, uint actual_addr)
 {
-	uint addr = br->instruction_addr;
-	uint index = addr & ((1 << BTB_BITSIZE) - 1);
-	uint shiftedindex = index * BTB_NUM_WAYS;
-	uint tag = addr >> BTB_BITSIZE;
 
 	if(br->is_call) {
 		lastcall = br->instruction_next_addr;
 	}
 
-	for (int i = 0; i < BTB_NUM_WAYS; i++) {
-		if (btb_tags[shiftedindex+i] == tag){
-
-			return;
-		}
-
-	}
-	
-	// insert into table
-	btb_tags[shiftedindex + btb_lru[index]] = tag;
-	btb_buffer[shiftedindex+ btb_lru[index]] = actual_addr;
-	btb_lru[index]++;
-	btb_lru[index] = btb_lru[index] % BTB_NUM_WAYS;
+	maincache->update(br->instruction_addr, actual_addr);
 }
 
 
@@ -75,39 +119,23 @@ void btb_setup(void)
 	char* str_numways = NULL;
 	str_bitsize = getenv("BTB_BITSIZE");
 	str_numways = getenv("BTB_NUM_WAYS");
+	int indexbits = 4;
+	int numways = 1;
 	if(str_bitsize)
-		BTB_BITSIZE = atoi(str_bitsize);
+		indexbits = atoi(str_bitsize);
 	if(str_numways)
-		BTB_NUM_WAYS = atoi(str_numways);
+		numways = atoi(str_numways);
 
-	BTB_SIZE = (1 << BTB_BITSIZE);
-	BTB_TAG_SIZE = (32 - BTB_BITSIZE);
-	fprintf(stderr, "%d entries by %d ways\n", BTB_SIZE, BTB_NUM_WAYS);
+	fprintf(stderr, "%d entries by %d ways\n", 1 << indexbits, numways);
 
-	btb_buffer = (uint*)malloc(BTB_SIZE*BTB_NUM_WAYS*sizeof(uint));
-	btb_tags = (uint*)malloc(BTB_SIZE*BTB_NUM_WAYS*sizeof(uint));
-	btb_lru = (uint*)malloc(BTB_SIZE*sizeof(uint));
+	maincache = new BTB_CACHE(indexbits, numways);
 
-	for(int i = 0; i < BTB_SIZE*BTB_NUM_WAYS; i++) {
-		btb_tags[i] = 0xffffffff;
-	}
-	for(int i = 0; i < BTB_SIZE; i++) {
-		btb_lru[i] = 0;
-	}
-
-	fprintf(stderr, "BTB size: %d\n",
-		BTB_SIZE*BTB_NUM_WAYS*BTB_TAG_SIZE + 
-		BTB_SIZE*BTB_NUM_WAYS*32+
-		BTB_SIZE*log2(2));
+	fprintf(stderr, "BTB size: %d\n",maincache->size());
 
 }
 
 void btb_destroy(void)
 {
-	free(btb_buffer);
-	free(btb_tags);
-	free(btb_lru);
-
+	delete maincache;
 }
 #endif
-
