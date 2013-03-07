5bd2b8c5dcf7b1df34d389d2ff797e1a92b9b41f
Andy Goetz <andy@andygoetz.org>
2013-03-07 15:50:51 -0800

    Added ability to store program counter displacements in btb instead of direct references

--- btb.cc
+++ btb.cc
@@ -24,11 +24,19 @@ private:
 	size_t numways;
 	size_t btbsize;
 	size_t tagsize;
+	int m_displacementbits;
 	uint* btb_buffer;
 	uint* btb_tags;
 	uint* btb_lru;
+
 public:
-	BTB_CACHE(int indexbits = 4, int numways = 1) : indexbits(indexbits), numways(numways), btbsize(1 << indexbits), tagsize(32-indexbits) {
+	int displacementbits() {
+		if (m_displacementbits < 0 || m_displacementbits >= 32)
+			return 32;
+		else
+			return m_displacementbits;
+	}
+	BTB_CACHE(int indexbits = 4, int numways = 1, int displacementbits = -1) : indexbits(indexbits), numways(numways), btbsize(1 << indexbits), tagsize(32-indexbits), m_displacementbits(displacementbits) {
 		btb_buffer = (uint*)malloc(btbsize*numways*sizeof(uint));
 		btb_tags = (uint*)malloc(btbsize*numways*sizeof(uint));
 		btb_lru = (uint*)malloc(btbsize*sizeof(uint));		
@@ -40,12 +48,12 @@ public:
 			btb_lru[i] = 0;
 		}
 	}
-	virtual ~BTB_CACHE() {
+	~BTB_CACHE() {
 		free(btb_buffer);
 		free(btb_tags);
 		free(btb_lru);
 	}
-	virtual bool predict(uint instr, uint &target) {
+	bool predict(uint instr, uint &target) {
 		uint index = instr & ((1 << indexbits) - 1);
 		uint shiftedindex = index * numways;
 		uint tag = instr >> indexbits;
@@ -58,14 +66,22 @@ public:
 		}
 		return false;
 	}
-	virtual void update(uint addr, uint target) {
+	bool update(uint addr, uint target) {
 		uint index = addr & ((1 << indexbits) - 1);
 		uint shiftedindex = index * numways;
 		uint tag = addr >> indexbits;
 		
+		if (displacementbits() != 32) {
+			uint delta = target - addr;
+			int64_t maxdisp = ((int64_t)1 << (m_displacementbits - 1)) - 1;
+			int64_t mindisp = -maxdisp - 1;
+			if (delta > maxdisp || delta < mindisp)
+				return false;
+		}
+		
 		for (uint i = 0; i < numways; i++) {
 			if (btb_tags[shiftedindex+i] == tag){
-				return;
+				return true;
 			}
 		}	
 
@@ -74,12 +90,13 @@ public:
 		btb_buffer[shiftedindex+ btb_lru[index]] = target;
 		btb_lru[index]++;
 		btb_lru[index] = btb_lru[index] % numways;
+		return true;
 	}
 
-	virtual uint size(void) {
+	uint size(void) {
 		uint size = 0;
 		size += btbsize * numways * tagsize;
-		size += btbsize * numways* 32;
+		size += btbsize * numways* displacementbits();
 		if(numways > 1)
 			size += btbsize*log2(numways);
 		return size;
@@ -111,24 +128,26 @@ void btb_update(const branch_record_c *br, uint actual_addr)
 	maincache->update(br->instruction_addr, actual_addr);
 }
 
-
+void getparam(const char* name, int *value) {
+	char* tmpval = NULL;
+	tmpval = getenv(name);
+	if (tmpval)
+		*value = atoi(tmpval);
+}
 // setup and destroy functions for the btb predictor
 void btb_setup(void)
 {
-	char* str_bitsize = NULL;
-	char* str_numways = NULL;
-	str_bitsize = getenv("BTB_BITSIZE");
-	str_numways = getenv("BTB_NUM_WAYS");
 	int indexbits = 4;
 	int numways = 1;
-	if(str_bitsize)
-		indexbits = atoi(str_bitsize);
-	if(str_numways)
-		numways = atoi(str_numways);
+	int dispsize = -1;
+	getparam("BTB_BITSIZE", &indexbits);
+	getparam("BTB_NUM_WAYS", &numways);
+	getparam("BTB_DISP_SIZE", &dispsize);
+		
 
-	fprintf(stderr, "%d entries by %d ways\n", 1 << indexbits, numways);
-
-	maincache = new BTB_CACHE(indexbits, numways);
+	maincache = new BTB_CACHE(indexbits, numways, dispsize);
+	fprintf(stderr, "%d entries by %d ways, %d bit displacements\n", 
+		1 << indexbits, numways, maincache->displacementbits());
 
 	fprintf(stderr, "BTB size: %d\n",maincache->size());
 
