9e5d19f4a27f7e434439875a7ea2b77cc80a21a2
Andy Goetz <calcfreak83@gmail.com>
2013-03-18 10:37:39 -0700

    commented out bullshit

--- runall.sh
+++ runall.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 sum=0
 for i in `ls traces/ | grep -v bz2 | cut -d. -f1 ` ; do 
-    echo $i 
-    tmpval=$(./launcher.pl predictor 1,6,8,6,2,8,21 traces/$i | grep tpred | perl -ne '@d = split / +/; print $d[8]')
+    #echo $i 
+    result=$(./launcher.pl predictor 1,6,8,-6,2,8,64 traces/$i)
+    #echo -n "$result"
+    tmpval=$( echo -n "$result" | grep tpred | perl -ne '@d = split /\s+/; print $d[8]')
     sum=$(echo "scale=5; $sum + $tmpval" | bc)
-    echo $sum
+#    echo $sum
 done
 
-print $(echo "scale=5; $sum / 20" | bc)
+echo $(echo "scale=5; $sum / 20" | bc)
