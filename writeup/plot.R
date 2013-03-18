1a9b4ece66d92383913229c704db285f0cf1250e
Andy Goetz <andy@andygoetz.org>
2013-03-18 16:15:51 -0700

    Added mo shit fo the wookiee to deal with

--- writeup/plot.R
+++ writeup/plot.R
@@ -7,12 +7,12 @@ dev.off()
 
 pdf("ecdf-serv-1-stack.pdf")
 sdata <- read.table('datasets/serv-1-stack-depth.gz')
-plot(ecdf(sdata[[1]]), main="", xlab="Stack Depth",  verticals=T, pch=46)
+plot(ecdf(sdata[[1]]), main="", xlab="Stack Depth",ylab="ECDF",  verticals=T, pch=46)
 dev.off()
 
 pdf("ecdf-fp-1-stack.pdf")
 fpdata <- read.table('datasets/fp-1-stack-depth.gz')
-plot(ecdf(fpdata[[1]]), main="", xlab="Stack Depth", verticals=T, pch=46)
+plot(ecdf(fpdata[[1]]), main="", xlab="Stack Depth",ylab='ECDF', verticals=T, pch=46)
 dev.off()
 
 pdf("maxdepth.pdf")
@@ -22,7 +22,7 @@ dev.off()
 
 pdf("ecdf-serv-delta.pdf")
 ddata <- read.table('datasets/delta_data.gz')
-plot(ecdf(ddata[[1]]), main="", xlab="Jump Displacement", verticals=T, pch=46)
+plot(ecdf(ddata[[1]]), main="", xlab="Jump Displacement", ylab='ECDF',verticals=T, pch=46)
 dev.off()
 
 pdf("btype.pdf")
